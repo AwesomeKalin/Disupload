87f28d0de8a2536b309802a6b9b9fce8cd938ac6
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-02-05 20:07:29 +0000

    Favicon and Options

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import { discordBot } from './discordBot.js';
+import fs from 'fs';
 
 // The frontend
 export class httpServer {
@@ -9,7 +10,6 @@ export class httpServer {
     favicon: Buffer;
     styleCSS: string;
     server: http.Server;
-    __dirname: string;
 
     constructor(port: number, bot: discordBot) {
         this.port = port;
@@ -17,6 +17,11 @@ export class httpServer {
     }
 
     // Start the server
+
+    loadStaticFiles() {
+        this.favicon = fs.readFileSync(`${__dirname}/../static/favicon.png`);
+    }
+
     start() {
         this.server = http.createServer(this.requestHandler.bind(this))
         this.server.listen(this.port, () => {
@@ -26,5 +31,23 @@ export class httpServer {
 
     async requestHandler(req: any, res: any) {
         console.log(req.method + ': ' + req.url);
+
+        try {
+            if(req.url === '/favicon.png') {
+                res.writeHead(200);
+                res.end(this.favicon);
+            } else if (req.method === 'OPTIONS') {
+                res.writeHead(200, {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS, DELETE',
+                    'Access-Control-Allow-Headers': 'Content-Type, Content-Disposition',
+                    'Access-Control-Max-Age': 86400,
+                    'Content-Length': 0,
+                })
+                res.end()
+            }
+        } catch {
+
+        }
     }
 }
