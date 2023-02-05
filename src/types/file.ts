5355537e1487f233d004f8089725d905f23b4766
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-02-05 20:40:36 +0000

    File Class

---
+++ src/types/file.ts
@@ -0,0 +1,35 @@
+// An actual file
+
+import { filePart } from "./filePart.js";
+import { v4 as uuidv4 } from 'uuid';
+
+class file {
+    name: string;
+    parts: [filePart];
+    uuid: string;
+
+    constructor (name: string, uuid: string) {
+        if (uuid === undefined) {
+            this.uuid = uuidv4();
+        } else {
+            this.uuid = uuid;
+        }
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getPartsList() {
+        return this.parts;
+    }
+
+    getPart(id: number) {
+        return this.parts[id];
+    }
+
+    getUUID() {
+        return this.uuid;
+    }
+}
