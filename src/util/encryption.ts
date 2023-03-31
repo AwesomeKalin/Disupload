dbded5614d929e026e32318c7164fd6b844e17b4
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-03-31 13:46:29 +0000

    v0.1.3
    Almost forgot one thing

--- src/util/encryption.ts
+++ src/util/encryption.ts
@@ -13,8 +13,7 @@ export async function encryptBuffer(buffer: Buffer, key: string) {
 
 // Decrypt a buffer using a given secret key
 export async function decryptBuffer(b: Buffer, key: string) {
-    const encrypted: Uint8Array = b.subarray(b.byteOffset, b.byteOffset + b.byteLength);
-    const message: openpgp.Message<Buffer> = await openpgp.readMessage({ binaryMessage: encrypted });
+    const message: openpgp.Message<Buffer> = await openpgp.readMessage({ binaryMessage: b });
     const { data: decrypted } = await openpgp.decrypt({
         message,
         passwords: [key],
