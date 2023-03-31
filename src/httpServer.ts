9e03481abf1f9a23ada30eaca64d4a57885f1cdb
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-03-31 13:45:41 +0000

    v0.1.3

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -96,14 +96,19 @@ export class httpServer {
                         // Typescript-ified and modified to work with my code by AwesomeKalin55
                         await new Promise((resolve, reject) => {
                             https.get(partsOfFile[j].getUrl(), (res2) => {
-                                res2.pipe(new AsyncStreamChunker(async (data) => {
+                                let data = [];
+                                let buffer: Buffer;
+                                res2.on('data', (chunk) => {
+                                    data.push(chunk);
+                                }).on('end', async () => {
+                                    buffer = Buffer.concat(data);
                                     if (this.password != undefined) {
-                                        data = await decryptBuffer(data, this.password);
+                                        buffer = await decryptBuffer(buffer, this.password);
                                     }
                                     if (!res.write(data)) await new Promise((r) => res.once('drain', r));
-                                }))
-                                res2.on('error', (err) => reject(err));
-                                res2.on('end', () => resolve(''));
+                                    resolve('');
+                                    res2.on('error', (err) => reject(err));
+                                });
                             });
                         });
                         // Credit ends here
